64508bceb90f571afae426403e1c5beae8a74a0f
Vani4ka89 <ivan.tym4ak@gmail.com>
2023-02-04 17:36:09 +0200

    new project js lesson_1

---
+++ lesson_1/js/main.js
@@ -0,0 +1,95 @@
+
+    ////////////////////////////////////Exercise_1//////////////////////////////
+    let users = ['Mykola', 6, 'Petro', false, 'Ira', 9, 'Olya', true, 7, 'Dmytro'];
+    console.log(users[0]);
+    console.log(users[1]);
+    console.log(users[2]);
+    console.log(users[3]);
+    console.log(users[4]);
+    console.log(users[5]);
+    console.log(users[6]);
+    console.log(users[7]);
+    console.log(users[8]);
+    console.log(users[9]);
+
+    ////////////////////////////////////Exercise_2/////////////////////////////
+    const book1 = {
+    title: 'Harry Potter',
+    pageCount: 74,
+    genre: 'fantastic'
+};
+    const book2 = {
+    title: 'Constitution of Ukraine',
+    pageCount: 12,
+    genre: 'law'
+};
+    const book3 = {
+    title: 'Chimera',
+    pageCount: 7,
+    genre: 'detective'
+};
+    console.log(book1);
+    console.log(book2);
+    console.log(book3);
+
+    ////////////////////////////////////Exercise_3////////////////////////
+    const book4 = {
+    title: 'Sato',
+    pageCount: 46,
+    genre: 'thriller',
+    authors: [
+{name: 'Rahim', age: 34}
+    ]
+};
+    const book5 = {
+    title: 'Doctor cypher',
+    pageCount: 8,
+    genre: 'fantasy',
+    authors: [
+{name: 'Roma', age: 29}
+    ]
+};
+    const book6 = {
+    title: 'Gait',
+    pageCount: 3,
+    genre: 'fantasy',
+    authors: [
+{name: 'Anna', age: 27}
+    ]
+};
+    console.log(book4);
+    console.log(book5);
+    console.log(book6);
+
+    ///////////////////////////////////////Exercise_4////////////////////////////////////
+    let allUsers = [
+    {name: 'Petro', username: 'petya23', password: 558521},
+    {name: 'Katya', username: 'ketri19', password: 1439423},
+    {name: 'Vasya', username: 'vaska24', password: 987627},
+    {name: 'Ira', username: 'irka22', password: 2367522},
+    {name: 'Kolya', username: 'kol4ik', password: 567525},
+    {name: 'Olha', username: 'oli4ka23', password: 76678528},
+    {name: 'Dima', username: 'dimon20', password: 143524},
+    {name: 'Anya', username: 'anyutka18', password: 143429},
+    {name: 'George', username: 'ge2223', password: 1434820},
+    {name: 'Joni', username: 'joni1990', password: 7678567}
+    ];
+    console.log(allUsers[0]['password']);
+    console.log(allUsers[1]['password']);
+    console.log(allUsers[2]['password']);
+    console.log(allUsers[3]['password']);
+    console.log(allUsers[4]['password']);
+    console.log(allUsers[5]['password']);
+    console.log(allUsers[6]['password']);
+    console.log(allUsers[7]['password']);
+    console.log(allUsers[8]['password']);
+    console.log(allUsers[9]['password']);
+
+    /////////////////////////////////////////Exercise_5/////////////////////////////////////////////
+    let x = 34;
+    if (x !== 0) {
+    console.log('ВІРНО');
+} else {
+    console.log('НЕ ВІРНО');
+}
+
