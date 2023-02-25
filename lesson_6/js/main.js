ae2b197cb1706f48125ef7de76a640ae50d03ed2
Vani4ka89 <ivan.tym4ak@gmail.com>
2023-02-25 18:15:34 +0200

    js lesson_1 homework done js lesson_2 homework done js lesson_3 homework done js lesson_4 homework done js lesson_5 homework done js lesson_6 homework done

--- lesson_6/js/main.js
+++ lesson_6/js/main.js
@@ -82,22 +82,131 @@ console.log(coursesAndDurationArray.filter(item => item.monthDuration > 5));
 
 // -- за допомоги map перетворити кожен елемент на наступний тип {id,title,monthDuration}
 
-console.log(coursesAndDurationArray.map((item, index) => ({id: index + 1, ...item})));
+let durMap = coursesAndDurationArray.map((value, index) => {
+    return {...value, id: index + 1};
+})
+console.log(durMap);
 
 // =========================
 //     описати колоду карт (від 6 до туза без джокерів)
+
+let cards = [
+    {cardSuit: 'heart', value: 6, color: 'red'},
+    {cardSuit: 'heart', value: 7, color: 'red'},
+    {cardSuit: 'heart', value: 8, color: 'red'},
+    {cardSuit: 'heart', value: 9, color: 'red'},
+    {cardSuit: 'heart', value: 10, color: 'red'},
+    {cardSuit: 'heart', value: 'jack', color: 'red'},
+    {cardSuit: 'heart', value: 'queen', color: 'red'},
+    {cardSuit: 'heart', value: 'king', color: 'red'},
+    {cardSuit: 'heart', value: 'ace', color: 'red'},
+
+
+    {cardSuit: 'spade', value: 6, color: 'black'},
+    {cardSuit: 'spade', value: 7, color: 'black'},
+    {cardSuit: 'spade', value: 8, color: 'black'},
+    {cardSuit: 'spade', value: 9, color: 'black'},
+    {cardSuit: 'spade', value: 10, color: 'black'},
+    {cardSuit: 'spade', value: 'jack', color: 'black'},
+    {cardSuit: 'spade', value: 'queen', color: 'black'},
+    {cardSuit: 'spade', value: 'king', color: 'black'},
+    {cardSuit: 'spade', value: 'ace', color: 'black'},
+
+
+    {cardSuit: 'diamond', value: 6, color: 'red'},
+    {cardSuit: 'diamond', value: 7, color: 'red'},
+    {cardSuit: 'diamond', value: 8, color: 'red'},
+    {cardSuit: 'diamond', value: 9, color: 'red'},
+    {cardSuit: 'diamond', value: 10, color: 'red'},
+    {cardSuit: 'diamond', value: 'jack', color: 'red'},
+    {cardSuit: 'diamond', value: 'queen', color: 'red'},
+    {cardSuit: 'diamond', value: 'king', color: 'red'},
+    {cardSuit: 'diamond', value: 'ace', color: 'red'},
+
+
+    {cardSuit: 'clubs', value: 6, color: 'black'},
+    {cardSuit: 'clubs', value: 7, color: 'black'},
+    {cardSuit: 'clubs', value: 8, color: 'black'},
+    {cardSuit: 'clubs', value: 9, color: 'black'},
+    {cardSuit: 'clubs', value: 10, color: 'black'},
+    {cardSuit: 'clubs', value: 'jack', color: 'black'},
+    {cardSuit: 'clubs', value: 'queen', color: 'black'},
+    {cardSuit: 'clubs', value: 'king', color: 'black'},
+    {cardSuit: 'clubs', value: 'ace', color: 'black'}
+];
+
 // - знайти піковий туз
+let spadeTuz = cards.find((item) => {
+     return item.cardSuit === 'spade' && item.value === 'ace';
+})
+console.log(spadeTuz);
+
 // - всі шістки
+
+console.log(cards.filter(card => card.value === 6));
+
 // - всі червоні карти
+
+console.log(cards.filter(card => card.color === 'red'));
+
 // - всі буби
+
+console.log(cards.filter(card => card.cardSuit === 'diamond'));
+
 // - всі трефи від 9 та більше
+
+console.log(cards.filter(card => card.cardSuit === 'clubs' && card.value > 8));
+
 //
 // {
 //     cardSuit: '', // 'spade', 'diamond','heart', 'clubs'
 //         value: '', // '6'-'10', 'ace','jack','queen','king','joker'
 //     color:'', // 'red','black'
 // }
+//let cards = [
+//     {cardSuit: 'heart', value: 6, color: 'red'},
+//     {cardSuit: 'heart', value: 7, color: 'red'},
+//     {cardSuit: 'heart', value: 8, color: 'red'},
+//     {cardSuit: 'heart', value: 9, color: 'red'},
+//     {cardSuit: 'heart', value: 10, color: 'red'},
+//     {cardSuit: 'heart', value: 'jack', color: 'red'},
+//     {cardSuit: 'heart', value: 'queen', color: 'red'},
+//     {cardSuit: 'heart', value: 'king', color: 'red'},
+//     {cardSuit: 'heart', value: 'ace', color: 'red'},
 //
+//
+//     {cardSuit: 'spade', value: 6, color: 'black'},
+//     {cardSuit: 'spade', value: 7, color: 'black'},
+//     {cardSuit: 'spade', value: 8, color: 'black'},
+//     {cardSuit: 'spade', value: 9, color: 'black'},
+//     {cardSuit: 'spade', value: 10, color: 'black'},
+//     {cardSuit: 'spade', value: 'jack', color: 'black'},
+//     {cardSuit: 'spade', value: 'queen', color: 'black'},
+//     {cardSuit: 'spade', value: 'king', color: 'black'},
+//     {cardSuit: 'spade', value: 'ace', color: 'black'},
+//
+//
+//     {cardSuit: 'diamond', value: 6, color: 'red'},
+//     {cardSuit: 'diamond', value: 7, color: 'red'},
+//     {cardSuit: 'diamond', value: 8, color: 'red'},
+//     {cardSuit: 'diamond', value: 9, color: 'red'},
+//     {cardSuit: 'diamond', value: 10, color: 'red'},
+//     {cardSuit: 'diamond', value: 'jack', color: 'red'},
+//     {cardSuit: 'diamond', value: 'queen', color: 'red'},
+//     {cardSuit: 'diamond', value: 'king', color: 'red'},
+//     {cardSuit: 'diamond', value: 'ace', color: 'red'},
+//
+//
+//     {cardSuit: 'clubs', value: 6, color: 'black'},
+//     {cardSuit: 'clubs', value: 7, color: 'black'},
+//     {cardSuit: 'clubs', value: 8, color: 'black'},
+//     {cardSuit: 'clubs', value: 9, color: 'black'},
+//     {cardSuit: 'clubs', value: 10, color: 'black'},
+//     {cardSuit: 'clubs', value: 'jack', color: 'black'},
+//     {cardSuit: 'clubs', value: 'queen', color: 'black'},
+//     {cardSuit: 'clubs', value: 'king', color: 'black'},
+//     {cardSuit: 'clubs', value: 'ace', color: 'black'}
+// ];
 // =========================
 //
 //     Взяти описану колоду карт, та за допомоги reduce упакувати всі карти по "мастях" в об'єкт
