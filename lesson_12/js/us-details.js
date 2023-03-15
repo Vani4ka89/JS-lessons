738780c5e636b50ee1c767a476686233d9a12785
Vani4ka89 <ivan.tym4ak@gmail.com>
2023-03-15 15:39:49 +0200

    made lesson_12... start to do mini_project....

--- lesson_12/js/us-details.js
+++ lesson_12/js/us-details.js
@@ -4,28 +4,23 @@ let userId = url.searchParams.get('user_id');
 fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
     .then(value => value.json())
     .then(user => {
-        console.log(user);
-        const infoWithUser = [];
 
-        function getInfo (user) {
+        function getInfo(user) {
             for (const key in user) {
                 if (typeof user[key] === 'object') {
                     getInfo(user[key])
                 } else {
-                    infoWithUser.push(`${key}: ${user[key]}`)
+                    const infoBox = document.getElementsByClassName('box-info')[0];
+                    const list = document.createElement('ul');
+                    const item = document.createElement('li');
+                    item.innerText = `${key}: ${user[key]}`;
+                    list.appendChild(item);
+                    infoBox.appendChild(list);
                 }
             }
         }
-        getInfo(user);
-        console.log(infoWithUser);
 
-        const infoBox = document.getElementsByClassName('box-info')[0];
-        const list = document.createElement('ul');
-        infoBox.appendChild(list);
-        for (const info of infoWithUser) {
-            const item = document.createElement('li');
-            item.innerText = `${info}`;
-            list.appendChild(item);
-        }
+        getInfo(user);
+
 
     })
